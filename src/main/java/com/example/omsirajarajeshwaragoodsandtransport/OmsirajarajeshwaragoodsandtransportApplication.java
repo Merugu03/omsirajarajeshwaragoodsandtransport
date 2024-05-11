44a31fc5a7c3bf41cea7b81affbcb9e42db93363
Merugu Akshay Kumar <meruguakshay@gmail.com>
2024-05-11 14:54:38 +0530

    Initial commit: Setup Spring Boot project

---
+++ src/main/java/com/example/omsirajarajeshwaragoodsandtransport/OmsirajarajeshwaragoodsandtransportApplication.java
@@ -0,0 +1,13 @@
+package com.example.omsirajarajeshwaragoodsandtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsirajarajeshwaragoodsandtransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsirajarajeshwaragoodsandtransportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omsirajarajeshwaragoodsandtransport/OmsirajarajeshwaragoodsandtransportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omsirajarajeshwaragoodsandtransport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsirajarajeshwaragoodsandtransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
